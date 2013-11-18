1e5d636b8d74820ac5ca5fe9c449615ed88a329b
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2013-11-18 19:38:32 +0100

    Fix enum tests

--- cdborm/tests/enumfield.py
+++ cdborm/tests/enumfield.py
@@ -3,55 +3,65 @@ from cdborm.model import Model
 from cdborm.fields import EnumField
 from cdborm.errors import FieldValidationError
 
-_default_value = 'default test'
-_default_value2 = 'default static'
+_default_value = 'second'
+_default_value2 = 'first'
+
 
 def make_default(value, parent):
     return _default_value
 
 enum_list = ['first', 'second']
 
-class MyStringModel(Model):
+
+class MyStringModel1(Model):
     first = EnumField(enum_list)
-    second = EnumField(enum_list, nullable=False)
-    third = EnumField(enum_list, default=make_default)
-    fourth = EnumField(enum_list, _default_value2)
+    second = EnumField(enum_list)
 
     @property
     def name(self):
         return self.first
 
 
+class MyStringModel2(Model):
+    second = EnumField(enum_list, nullable=False)
+
+
+class MyStringModel3(Model):
+    third = EnumField(enum_list, default=make_default)
+    fourth = EnumField(enum_list, _default_value2)
+
+
 class EnumFieldTest(CdbOrmTestCase):
 
     def test_simple_assigning(self):
-        value = u'string test'
-        obj = MyStringModel()
+        value = enum_list[0]
+        obj = MyStringModel1()
         obj.first = value
 
         self.assertEqual(value, obj.first)
 
-    def test_string_assiging_success(self):
-        obj = MyStringModel()
-        obj.first = 'str to unicode'
+    def test_string_assiging_fail(self):
+        obj = MyStringModel1()
+        def fun():
+            obj.first ='str'
 
-        self.assertEqual(u'str to unicode', obj.first)
+        self.assertRaises(ValueError, fun)
 
     def test_save_and_restore(self):
-        value = u'good'
-        obj = MyStringModel()
+        value = enum_list[0]
+        obj = MyStringModel1()
         obj.second = value
         obj.save()
 
         Model.cache = {}
 
-        obj2 = MyStringModel.get(obj.id)
+        obj2 = MyStringModel1.get(obj.id)
 
         self.assertEqual(value, obj2.second)
         self.assertEqual(None, obj2.first)
 
     def test_field_validation_error(self):
-        obj = MyStringModel()
+        obj = MyStringModel2()
         try:
             obj.save()
         except FieldValidationError, er:
@@ -61,14 +71,14 @@ class EnumFieldTest(CdbOrmTestCase):
     def test_bad_assign(self):
         def bad_assign(obj):
             obj.first = 123
-        #-----------------------------------------------------------------------
-        obj = MyStringModel()
+        #----------------------------------------------------------------------
+        obj = MyStringModel1()
         self.assertRaises(ValueError, bad_assign, obj)
 
     def test_initial_data(self):
-        first = 'something 1'
-        second = 'something 2'
-        obj = MyStringModel(
+        first = enum_list[0]
+        second = enum_list[1]
+        obj = MyStringModel1(
             first=first,
             second=second,
         )
@@ -83,9 +93,9 @@ class EnumFieldTest(CdbOrmTestCase):
         self.assertEqual(obj.name, first)
 
     def test_to_dict(self):
-        first = 'something 1'
-        second = 'something 2'
-        obj = MyStringModel(
+        first = 'first'
+        second = 'second'
+        obj = MyStringModel1(
             first=first,
             second=second,
         )
@@ -98,27 +108,27 @@ class EnumFieldTest(CdbOrmTestCase):
 
     def test_from_dict(self):
         data = {
-            'first': '1 something 1',
-            'second': '1 something 2',
-            '_type': MyStringModel.__name__,
+            'first': 'first',
+            'second': 'second',
+            '_type': MyStringModel1.__name__,
             '_type_version': 1,
         }
-        obj = MyStringModel.from_dict(data)
+        obj = MyStringModel1.from_dict(data)
 
         self.assertEqual(data['first'], obj.first)
         self.assertEqual(data['second'], obj.second)
 
     def test_init(self):
         data = {
-            'first': '1 something 1',
-            'second': '1 something 2',
+            'first': 'first',
+            'second': 'second',
         }
-        obj = MyStringModel(**data)
+        obj = MyStringModel1(**data)
 
         self.assertEqual(data['first'], obj.first)
         self.assertEqual(data['second'], obj.second)
 
     def test_default(self):
-        obj = MyStringModel()
+        obj = MyStringModel3()
         self.assertEqual(_default_value, obj.third)
         self.assertEqual(_default_value2, obj.fourth)
